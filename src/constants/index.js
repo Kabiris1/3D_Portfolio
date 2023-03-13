590f687b6b965455b0ff943feb14b20d073bab5c
kabir <kabir.almuslim@gmail.com>
2023-03-13 12:33:51 +0530

    new

--- src/constants/index.js
+++ src/constants/index.js
@@ -108,7 +108,7 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      "I thought it was impossible to make a website as beautiful as our product, but Abdul Karam proved me wrong.",
     name: "Sri Nagesh Kotipalli",
     designation: "CFO",
     company: "SkyGoal",
